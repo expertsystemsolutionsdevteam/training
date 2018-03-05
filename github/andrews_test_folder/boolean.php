3591aa6bea3911d0e37ce94f59cbcc4f3aeb5c73
AndrewIsraelsen <35880465+AndrewIsraelsen@users.noreply.github.com>
2018-03-04 19:21:44 -0700

    Boolean test

---
+++ github/andrews_test_folder/boolean.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Boolean Test</title>
+</head>
+<body>
+
+	<?php
+
+		$boolean = 0;
+
+		if($boolean == 1) {
+
+			echo "You are Great!";
+
+		} elseif($boolean == 0) {
+
+			echo "You are not great...";
+
+		} else {
+
+			echo "I'm not even sure...";
+
+		}
+
+	?>
+
+</body>
+</html>
